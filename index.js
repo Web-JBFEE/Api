18e374fb7c59d4470073aa4039d4401697e5d7ba
Naufal Adli santosa <adlisantosanaufal@gmail.com>
2024-11-06 19:41:08 +0700

    oke

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// // index.js
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const app = express();
+
+// // Middleware untuk parsing JSON
+// app.use(express.json());
+
+// // Import routes
+// const routes = require('./routers/routers');
+
+// // Gunakan routes dengan prefix `/api`
+// app.use('/api', routes);
+
+// // Menjalankan server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 // index.js
 
 const express = require('express');
@@ -15,8 +39,7 @@ const routes = require('./routers/routers');
 // Gunakan routes dengan prefix `/api`
 app.use('/api', routes);
 
-// Menjalankan server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Menjalankan server pada IP 0.0.0.0 dan port 3000
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
